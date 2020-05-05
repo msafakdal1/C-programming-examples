885e126e9fef518168a3188cb2d35764737d3a6d
Melih Safak <64839032+msafakdal1@users.noreply.github.com>
2020-05-05 22:48:13 +0300

    Create -1'e kadar topla.c

---
+++ -1'e kadar topla.c
@@ -0,0 +1,16 @@
+
+int main()
+{
+    int sayı;
+    int toplam=0;
+
+   while(sayı!=-1){
+       printf("Sayı giriniz:");     scanf("%d",&sayı);
+       toplam+=sayı;
+   }
+   
+   printf("Toplam:%d",toplam);
+   return 0;
+}
+
+
