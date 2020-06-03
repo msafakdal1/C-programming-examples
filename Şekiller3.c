722d379c4d764dd655cb5c73c0a422bf71b977ba
Melih Safak <64839032+msafakdal1@users.noreply.github.com>
2020-06-03 14:22:15 +0300

    Create Şekiller3.c

---
+++ Şekiller3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("PPPPPPPPP\n");
+    printf("    P   P\n");
+    printf("    P   P\n");
+    printf("    P   P\n");
+    printf("     P P\n");
+
+    printf("-------------------------------------------\n");
+
+
+    printf("    JJ\n");
+    printf("  J\n");
+    printf("J\n");
+    printf("  J\n");
+    printf("    JJJJJJJ\n");
+
+    printf("----------------------------------------------\n");
+
+    printf("DDDDDDDDD\n");
+    printf("D       D\n");
+    printf("D       D\n");
+    printf(" D     D\n");
+    printf("  DDDDD\n");
+
+    return 0;
+
+    
+}
