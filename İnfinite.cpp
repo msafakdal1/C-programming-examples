289ce70524d6aabedb562193bf4f061b61b8b9a4
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-06-27 23:12:16 +0300

    Create İnfinite.cpp
    N	10*N		100*N		1000*N
    1	10			100			1000
    2	20			200			2000
    3	30			300			3000
    4	40			400			4000
    5	50			500			5000

---
+++ İnfinite.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+
+    unsigned int count = 1;
+
+    cout<<"N\t10*N\t\t100*N\t\t1000*N"<<endl;
+
+
+    while(count<=5){
+        cout<<count<<"\t"<<10*count<<"\t\t\t"<<100*count<<"\t\t\t"<<1000*count<<endl;
+
+        ++count;
+    }
+
+}
+
+
+
+
+
+
+
