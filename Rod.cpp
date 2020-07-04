027322ba4de2a377dac772f94205fcce1d0c3a8a
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-07-04 08:47:57 +0300

    Create Rod.cpp

---
+++ Rod.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    int a , b, c ,d ,e;
+
+    cout<<"a , b, c , d ve e sayılarını giriniz:";          cin>>a>>b>>c>>d>>e;
+
+    for(int i=1;i<=a;++i){
+        cout<<"*";
+    }
+    cout<<endl<<endl;
+
+    for(int i=1;i<=b;++i){
+        cout<<"*";
+    }
+
+    cout<<endl<<endl;
+    for(int i=1;i<=c;++i){
+        cout<<"*";
+    }
+
+    cout<<endl<<endl;
+
+    for(int i=1;i<=d;++i){
+        cout<<"*";
+    }
+
+    cout<<endl<<endl;
+
+    for(int i=1;i<=e;++i){
+        cout<<"*";
+    }
+}
