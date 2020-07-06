ff112d0320bd2880e931b12e58f46618b6f74216
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-07-06 20:54:29 +0300

    Create veriTipleriTanımlama.c

---
+++ veriTipleriTanımlama.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("İnteger:%d\n",sizeof(int));
+    printf("Char:%d\n",sizeof(char));
+    printf("Float:%d\n",sizeof(float));
+    printf("Double:%d\n",sizeof(double));
+    printf("short:%d\n",sizeof(short));
+    printf("Long:%ld\n",sizeof(long));
+    printf("Unsigned:%d\n",sizeof(unsigned short));
+    printf("Unsigned Long:%ld\n",sizeof(unsigned long));
+    printf("Long double:%ld\n",sizeof(long double));
+
+}
