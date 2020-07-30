1160d41ac37b3a659c49c809c2fb7fabf7131613
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-07-30 19:01:10 +0300

    Create toplamAlınanYol.py

---
+++ toplamAlınanYol.py
@@ -0,0 +1,7 @@
+yakıtToplam = float(input("Toplam aktığınız yakıt miktarını giriniz :"))
+yolToplam = float(input("Kaç kilometre yol aldığınızı giriniz :"))
+
+toplamHarcama = yakıtToplam * yolToplam
+
+
+print("Toplam Harcama : {}".format(toplamHarcama))
