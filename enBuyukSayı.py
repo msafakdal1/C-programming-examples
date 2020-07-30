d12825cf03a2afbb485ddd7987873803c5802681
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-07-31 01:37:36 +0300

    Create enBuyukSayı.py

---
+++ enBuyukSayı.py
@@ -0,0 +1,16 @@
+a = int(input("a sayısını giriniz :"))
+b = int(input("b sayısını giriniz :"))
+c = int(input("c sayısını giriniz :"))
+
+max = 0
+
+
+if a>max:
+    max = a
+if b>max:
+    max = b
+if c>max:
+    max = c
+    
+    
+print("{} sayısı en büyük sayıdır".format(max))
