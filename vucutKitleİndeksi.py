f2c567340636469d4708b2f037353b3fdf438f25
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-07-30 18:57:58 +0300

    Create vucutKitleİndeksi.py

---
+++ vucutKitleİndeksi.py
@@ -0,0 +1,10 @@
+boy = float(input("Boyunuzu giriniz :"))
+kilo = float(input("Kilonuzu giriniz :"))
+
+bedenKitleİndeksi = kilo / (boy * boy)
+
+
+print("Beden Kitle İndeksiniz hesaplanıyor lütfen bekleyiniz :")
+
+
+print("Beden Kitle İndeksi : {}".format(bedenKitleİndeksi))
