571095d1767ae1074d188c6259f1062abffbb56a
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-07-30 19:03:28 +0300

    Create kisiselBilgiler.py

---
+++ kisiselBilgiler.py
@@ -0,0 +1,8 @@
+ad = input("Adınızı giriniz :")
+soyad = input("Soyadınızı giriniz :")
+numara = input("Numaranızı giriniz :")
+
+
+print("Bilgileriniz\n\n")
+
+print("Ad :{}\nSoyad :{}\nNumara :{}".format(ad,soyad,numara))
