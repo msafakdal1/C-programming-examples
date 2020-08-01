5b358ed6dc24bd2aae922423aa6e199be1790ea5
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-01 17:25:18 +0300

    Create girilenİkiSayıArasındakiSayılarınToplam.py

---
+++ girilenİkiSayıArasındakiSayılarınToplam.py
@@ -0,0 +1,11 @@
+sayı1 = int(input("Birinci sayıyı giriniz :"))
+sayı2 = int(input("İkinci sayıyı giriniz :"))
+
+toplam = 0
+
+for i in range(sayı1,sayı2+1):
+    toplam += i
+    
+
+print("Toplam :",toplam)    
+    
