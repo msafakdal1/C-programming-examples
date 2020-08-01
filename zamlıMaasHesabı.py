912024b1d3363fa31aec6e13375ac3015d60b345
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-01 18:05:13 +0300

    Create zamlıMaasHesabı.py

---
+++ zamlıMaasHesabı.py
@@ -0,0 +1,8 @@
+maas = float(input("Maasınızı giriniz :"))
+zam_oranı = float(input("Zam Oranınızı giriniz :"))
+
+
+toplam_maas = maas + maas * (zam_oranı/100)
+
+
+print("iscinin zamlı maası :",toplam_maas)
