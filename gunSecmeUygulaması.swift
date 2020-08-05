afbbff9cb0ee9e1a6b88a64a2d05b954e0f92a44
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-05 22:44:46 +0300

    Create gunSecmeUygulaması.swift

---
+++ gunSecmeUygulaması.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var gun = 4
+
+
+switch gun{
+case 1:
+    print("Pazartesi")
+case 2:
+    print("Salı")
+case 3:
+    print("Carşamba")
+case 4:
+    print("Perşembe")
+case 5:
+    print("Cuma")
+case 6:
+    print("Cumartesi")
+case 7:
+    print("Pazar")
+default:
+    print("Böyle bir gün yok")
+}
