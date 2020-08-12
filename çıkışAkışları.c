f6d13d900cc0c5e37ab249053d05c1f58f1764cc
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-12 09:24:33 +0300

    Create çıkışAkışları.c

---
+++ çıkışAkışları.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("\'Mehmet\'");
+  puts("");
+
+  printf("\"Mehmet\"");
+  puts("");
+  printf("\?Mehmet\?");
+ puts("");
+
+ printf("\\Mehmet\\");
+ puts("");
+
+ printf("\aMehmet\a");
+
+ puts("");
+
+ printf("\bMehmet\b");
+ puts("");
+ printf("Mehmet\nUner");
+
+ printf("\rMehmet\r");
+ puts("");
+ printf("M\te\th\tm\te\tt");
+
+
+}
