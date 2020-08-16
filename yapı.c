971d0b2db0f5cffd65830081763f515c6fa885ea
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-16 16:09:10 +0300

    Create yapı.c

---
+++ yapı.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  
+struct örnek{
+char c;
+int i;
+}sample1 , sample2;
+
+int main()
+{
+  printf("Bir değer giriniz :");        scanf("%d",&sample1.i);
+
+ printf("%d",sample1.i);
+}
