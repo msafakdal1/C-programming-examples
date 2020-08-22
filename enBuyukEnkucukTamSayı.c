021c7366cbb8fcec637c18e7b0ef15e5e67d8c83
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-22 12:26:05 +0300

    Create enBuyukEnkucukTamSayı.c

---
+++ enBuyukEnkucukTamSayı.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+  int sayı = INT_MAX;
+  int sayı2 = INT_MIN;
+
+  printf("%d\n",sayı);
+  printf("%d\n",sayı +  1);
+  printf("%d\n",sayı2 -  1);
+  printf("%d",sayı2);
+}
